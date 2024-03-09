36691bd552ed60ee2fc949d34d6fab28a45fe0fd
Laura <laura.kask@tktk.ee>
2024-03-09 09:36:58 +0200

    add week5 code

---
+++ OrOp/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= OR Chemistry >= 90 OR Bio >= 90
+//math && chem >= 90 || math && bio >= 90 || chem && bio >= 90
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
